4d016ceddf58291a38c582037724f42062f2112e
Tom <tomdriscoll5@gmail.com>
2019-09-10 14:16:04 -0400

    still heroku 2.0

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,7 +2,7 @@ class Upload < ApplicationRecord
     validates :name, :type, :path, :uploader_id, presence: true
 
     belongs_to :uploader,
-    class_name :User
+    class_name: :User
 
     IMAGE_SUFFIXES = ['.gif', '.jpg', '.png']
     def get_type
