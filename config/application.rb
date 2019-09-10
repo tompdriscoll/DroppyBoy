cc2056603ba2b9acffd5ec32992c0c022fa97f16
Tom <tomdriscoll5@gmail.com>
2019-09-10 12:51:42 -0400

    light css/ still trying w/ heroku

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ Bundler.require(*Rails.groups)
 module Droppyboy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
+    config.load_defaults 5.2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
