aa221afcab834fc3601cbbcd2562d0be93d3eb7c
Tom <tomdriscoll5@gmail.com>
2019-09-10 15:58:45 -0400

    upload util

---
+++ frontend/util/file_api_util.js
@@ -0,0 +1,37 @@
+export const fetchUploads = data => (
+    $.ajax({
+      method: 'GET',
+      url: 'api/uploads',
+      data
+    })
+  );
+  
+  export const fetchUpload = id => (
+    $.ajax({
+      method: 'GET',
+      url: `api/uploads/${id}`
+    })
+  );
+
+  export const createUpload = upload => (
+    $.ajax({
+      method: 'POST',
+      url: 'api/uploads',
+      upload
+    })
+  );
+
+  export const editUpload = upload => (
+      $.ajax ({
+          method: 'PATCH',
+          url: `api/uploads/${id}`,
+          upload
+      })
+  )
+
+  export const deleteUpdate = id => (
+      $.ajax ({
+          method: 'DELETE',
+          url: `api/uploads/${id}`
+      })
+  )
