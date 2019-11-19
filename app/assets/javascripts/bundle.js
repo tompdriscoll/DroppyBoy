90e62071d66d6f3a2b9b21b7b32946dc8ddfb904
Tom <tdriscoll1@yahoo.com>
2019-11-19 15:34:18 -0500

    added arrow scroll

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -1142,17 +1142,63 @@ var Grid = function Grid() {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
 
-var HeroHeader = function HeroHeader() {
-  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("header", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
-    className: "hero-title"
-  }, "Put your creative energy to work, with Droppyboy"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
-    className: "hero-copy"
-  }, "Droppyboy is a modern workspace designed to reduce busywork\u2014so you can focus on the things that matter."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
-    className: "arrow"
-  }));
-};
+
+var HeroHeader =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(HeroHeader, _React$Component);
+
+  function HeroHeader() {
+    _classCallCheck(this, HeroHeader);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(HeroHeader).apply(this, arguments));
+  }
+
+  _createClass(HeroHeader, [{
+    key: "arrowScroll",
+    value: function arrowScroll() {
+      var app = document.getElementById('grid2');
+      window.scrollTo({
+        top: app.offsetTop + 50,
+        behavior: 'smooth'
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("header", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+        className: "hero-title"
+      }, "Put your creative energy to work, with Droppyboy"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "hero-copy"
+      }, "Droppyboy is a modern workspace designed to reduce busywork\u2014so you can focus on the things that matter."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+        id: "arrow",
+        className: "arrow",
+        onClick: this.arrowScroll
+      }));
+    }
+  }]);
+
+  return HeroHeader;
+}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
 
 /* harmony default export */ __webpack_exports__["default"] = (HeroHeader);
 
@@ -31886,7 +31932,7 @@ function warning(message) {
 /*!***************************************************************!*\
   !*** ./node_modules/react-router-dom/esm/react-router-dom.js ***!
   \***************************************************************/
-/*! exports provided: BrowserRouter, HashRouter, Link, NavLink, MemoryRouter, Prompt, Redirect, Route, Router, StaticRouter, Switch, generatePath, matchPath, withRouter, __RouterContext */
+/*! exports provided: MemoryRouter, Prompt, Redirect, Route, Router, StaticRouter, Switch, generatePath, matchPath, withRouter, __RouterContext, BrowserRouter, HashRouter, Link, NavLink */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
--- frontend/components/splash/grid/hero_header.jsx
+++ frontend/components/splash/grid/hero_header.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
-const HeroHeader = () => (
-   <header>
-       <h1 className="hero-title">Put your creative energy to work, with Droppyboy</h1>
-       <p className="hero-copy">
-           Droppyboy is a modern workspace designed to reduce busywork—so you can focus on the things that matter.
-        </p>
-        <button className="arrow"></button>
-   </header>
-)
+class HeroHeader extends React.Component {
+   
+   arrowScroll(){
+      let app = document.getElementById('grid2')
+      window.scrollTo({
+        top: (app.offsetTop + 50),
+        behavior: 'smooth'
+      })
+    }
+
+   render (){
+      return(
+      <header>
+         <h1 className="hero-title">Put your creative energy to work, with Droppyboy</h1>
+         <p className="hero-copy">
+            Droppyboy is a modern workspace designed to reduce busywork—so you can focus on the things that matter.
+         </p>
+         <button id='arrow' className="arrow" onClick={this.arrowScroll}></button>
+      </header>)}
+}
 export default HeroHeader;
