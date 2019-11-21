7c25dbfff897c8cff95eb9542c34d38d54588364
Tom <tdriscoll1@yahoo.com>
2019-11-21 12:25:43 -0500

    fixed bottom links

--- frontend/components/splash/grid6/grid6.jsx
+++ frontend/components/splash/grid6/grid6.jsx
@@ -7,49 +7,40 @@ const Grid6 = () => (
                 <li>
                     <h6>Droppyboy</h6>
                     <ul className='small-list'>
-                        <li>Desktop app</li>
-                        <li>Mobile apps</li>
-                        <li>Plans</li>
-                        <li>Security</li>
-                        <li>Features</li>
+                        <li>A React app</li>
+                        <li>Built by</li>
+                        <li>Tom Driscoll</li>
+                        <li>Based on</li>
+                        <li>Dropbox</li>
                     </ul>
                 </li>
                 <li>
-                    <h6>Products</h6>
+                    <h6>Features</h6>
                     <ul className='small-list'>
-                        <li>Plus</li>
-                        <li>Professional</li>
-                        <li>Business</li>
-                        <li>Enterprise</li>
+                        <li>User Auth</li>
+                        <li>File Upload</li>
+                        <li>File Download</li>
+                        <li>File Preview</li>
+                        
+                    </ul>
+                </li>
+                <li>
+                    <h6>Technologies</h6>
+                    <ul className='small-list'>
+                        <li>React</li>
+                        <li>Ruby on Rails</li>
+                        <li>AWS Storage</li>
+                        <li>Node.js</li>
+                        <li>PostgreSQL</li>
                     </ul>
                 </li>
                 
                 <li>
-                    <h6>Company</h6>
+                    <h6>Toms Links</h6>
                     <ul className='small-list'>
-                        <li>About us</li>
-                        <li>Jobs</li>
-                        <li>Press</li>
-                        <li>Investor relations</li>
-                        <li>Blog</li>
-                    </ul>
-                </li>
-                <li>
-                    <h6>Support</h6>
-                    <ul className='small-list'>
-                        <li>Help center</li>
-                        <li>Contact us</li>
-                        <li>Cookies</li>
-                        <li>Privacy & terms</li>
-                        <li>Sitemap</li>
-                    </ul>
-                </li>
-                <li>
-                    <h6>Community</h6>
-                    <ul className='small-list'>
-                        <li>Developers</li>
-                        <li>Referrals</li>
-                        <li>Forum</li>
+                        <li>Toms GitHub</li>
+                        <li>Toms LinkedIn</li>
+                        <li>Toms AngelList</li>
                     </ul>
                 </li>
             </ul>
