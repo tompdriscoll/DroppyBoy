0f893073963ed27ffb181dd1c70ab0a85539690e
Tom <tomdriscoll5@gmail.com>
2020-02-24 13:24:40 -0500

    file icon component

---
+++ frontend/components/home/home_files.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import RecentItem from './recent_item'
+import SuggestedListItem from './suggested_list_item'
+
+import UploadForm from '../uploads/upload_from'
+const HomeMain = props => {  
+    const recentItems = props.files.map(file => (     
+        <RecentItem  handleDelete={props.handleDelete} file={file} />
+    )).reverse()
+
+   
+ 
+    return (
+    <div id='home-main'>
+        <div id='suggested' className='section'>
+            <h2 className='section-header'> Suggested for you
+
+                    
+
+            </h2>
+            <ul id='suggested-list'>
+                                         
+            </ul>
+        </div>
+        <div id='recent' className='section'>
+            <h2 className='section-header'> Recent
+
+                    {/* <button className="hide-button">
+                        <span className="">Hide</span>
+                    </button> */}
+
+            </h2>
+            <ul id='recent-list'>
+                {recentItems}       
+            </ul>
+            
+        </div>
+        <div id='starred' className='section'>
+            <h2 className='section-header'> Starred
+                
+             
+                
+            </h2>
+            <p>
+                When you star items, they’ll show up here for easy access. 
+                {/* <a href="/#/help/desktop-web/star-doc-file-folder" target="_blank" rel="noopener noreferrer"> Learn more</a> */}
+            </p>
+        </div>
+    </div>)
+   
+
+}
+
+export default HomeMain;
+
+
